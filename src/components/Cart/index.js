bf380b0e203b48fdfca4bcfc25b26ea96a805e75
aSilas <silastj@hotmail.com>
2020-08-02 00:05:24 -0300

    inicio do carrinho

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,22 +1,60 @@
-import React from 'react';
+import React, { useState} from 'react';
+import {useSelector} from 'react-redux';
 import {
     CartArea,
     CartHeader,
     CartIcon,
     CartText,
-    CartBody
+    CartBody,
+    ProductsArea,
+    ProductItem,
+    ProductPhoto,
+    ProductInfoArea,
+    ProductName,
+    ProductPrice,
+    ProductQuantArea,
+    ProductIcon,
+    ProductNumber
 } from './styled';
 
 export default () => {
+
+    const products = useSelector(state=> state.cart.products);
+
+    const [show, setShow] = useState(true) ;
+
+    const handleCardClick =() =>{
+        setShow(!show)
+    }
     return (
         <CartArea>
-            <CartHeader>
-                <CartIcon src="/assets/cart.png"/>
-                <CartText>Meu Carrinho</CartText>
-            </CartHeader>
-            <CartBody>
+            <CartHeader onClick={handleCardClick}>
+                <CartIcon src="/assets/cart.png"/>             
+                <CartText>Meu Carrinho({products.length})</CartText>
+                { show &&
+                    <CartIcon src="/assets/down.png" />
+                }{!show &&
+                    <CartIcon src="/assets/up.png"/>   
+                }             
+                </CartHeader>
+                <CartBody show={show}>
+                    <ProductsArea>
 
-            </CartBody>
+                        <ProductItem>
+                            <ProductPhoto src="/assets/cart.png"/>
+                            <ProductInfoArea>
+                                <ProductName>Bolo</ProductName>
+                                <ProductPrice>R$10,00</ProductPrice>
+                            </ProductInfoArea>
+                            <ProductQuantArea>
+                                <ProductIcon src="/assets/menos.png"/>
+                                    <ProductNumber>10</ProductNumber>
+                                <ProductIcon src="/assets/mais.png"/>
+                            </ProductQuantArea>
+
+                        </ProductItem>
+                    </ProductsArea>
+                </CartBody>
         </CartArea>
     );
 }
--- src/components/Cart/styled.js
+++ src/components/Cart/styled.js
@@ -5,9 +5,8 @@ export const CartArea = styled.div`
     background-color:#136713;
     position:fixed;
     bottom:0;
-    right:40px;
-    
-    height:51px;
+    right:40px;    
+    max-height:80vh;
     border-radius:10px 10px 0 0;
 `;
 
@@ -27,7 +26,42 @@ export const CartIcon = styled.img`
     margin:0 14px;
 `;
 export const CartText = styled.div`
+    flex:1;
     font-size:17px;
     color:white;
 `;
-export const CartBody = styled.div``;
+export const CartBody = styled.div`
+   display:${props=> props.show ? 'flex': 'none'};
+   transition: .4s ease;
+   border:1px solid red;
+   height:100px;
+
+`;
+export const ProductsArea = styled.div``;
+export const ProductItem = styled.div`
+    display:flex;
+`;
+export const ProductPhoto = styled.img`
+    width:64px;
+    margin:0 14px;
+`;
+export const ProductInfoArea = styled.div`
+    flex:1;
+`;
+export const ProductName = styled.div``;
+export const ProductPrice = styled.div``;
+export const ProductQuantArea = styled.div`
+    background-color:blue;
+    display:flex;
+    align-items:center;
+    width:100%;
+`;
+
+export const ProductIcon = styled.img`
+    width:100%;
+    height:auto;
+    cursor:pointer;
+`;
+export const ProductNumber = styled.div``;
+
+
